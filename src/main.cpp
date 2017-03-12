c5ba55e639923c835796399de9d8b26521ae6705
Konrad <szymoniak.konrad@gmail.com>
2017-03-12 20:40:09 +0100

    modify main to use Engine class, program renders example cube

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,11 @@
-#include <iostream>
-#include <string>
-
-#define GLEW_STATIC
-#include <GL/glew.h>
-
-#include <GLFW/glfw3.h>
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-
-const GLuint WINDOW_WIDTH = 800;
-const GLuint WINDOW_HEIGHT = 600;
+#include "Engine.h"
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    dummy::Engine app;
 
-    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "oglapp", nullptr, nullptr);
-    if (!window)
-    {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK)
-    {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
-
-    int width;
-    int height;
-    glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
-
-    double lastTime = glfwGetTime();
-    int fps = 0;
-
-    while (!glfwWindowShouldClose(window))
-    {
-        double currentTime = glfwGetTime();
-        ++fps;
-        if (currentTime - lastTime >= 1.0)
-        {
-            std::string title = std::to_string(fps) + " FPS";
-            glfwSetWindowTitle(window, title.c_str());
-            fps = 0;
-            lastTime = glfwGetTime();
-        }
-
-        glfwPollEvents();
-
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-    }
-
-    glfwTerminate();
+    // run game loop
+    app.run();
 
     return 0;
-}
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
 }
